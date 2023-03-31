b2f924b4d01f4457960948f7714544bd274f57d6
NZelaya24 <825123@mystma.org>
2023-03-31 14:33:17 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #WRITE YOUR CODE IN THIS FILE
+def countA (x):
+    numA = 0
+    for i in range(0, len (x)):
+        if x[i] == "a":
+            numA = numA + 1
+    return numA
+
+
+countA ("cat")
+countA ("dog")
+countA ("armadillo")
+countA ("astronaut")
